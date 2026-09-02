2c872f2c029166593862fd8ce0993a883d053796
Muhammad Asif Khan <muasifkn@gmail.com>
2026-09-01 23:18:38 -0700

    Amazon job added.

--- muasifk.github.io old sep2024/scripts.js
+++
@@ -1,7 +0,0 @@
-var menuIcon = document.querySelector('.menu-icon');
-var nav = document.querySelector('nav ul');
-
-menuIcon.addEventListener('click', function() {
-  menuIcon.classList.toggle('active');
-  nav.classList.toggle('active');
-});
